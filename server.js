0ecb4e4012038351a036115f6ab0ab03323d7778
dezimba <omararnars@gmail.com>
2019-04-05 21:15:55 +0000

    heroku

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
+const {
+  PORT: port = 3000,
+} = process.env;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -20,7 +23,7 @@ app
       return handle(req, res)
     });
 
-    server.listen(3000, err => {
+    server.listen(port, err => {
       if (err) throw err;
       console.log('> Ready on http://localhost:3000');
     });
