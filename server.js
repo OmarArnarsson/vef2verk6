967e4fdf2a27d588f0f85ed77751d565e6f861c1
dezimba <omararnars@gmail.com>
2019-04-05 21:34:19 +0000

    loka tilraun heroku

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const {
   PORT: port = 3000,
   HOST: host = '127.0.0.1',
+  DATABASE_URL: data = 'postgres://notandi:mypass@localhost/v2',
 } = process.env;
 const app = next({ dev });
 const handle = app.getRequestHandler();
