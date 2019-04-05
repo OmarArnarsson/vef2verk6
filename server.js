31b126a02e3e7751c8226f408c188bd040cafbe5
dezimba <omararnars@gmail.com>
2019-04-05 21:22:31 +0000

    heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 const {
   PORT: port = 3000,
+  HOST: host = '127.0.0.1',
 } = process.env;
 const app = next({ dev });
 const handle = app.getRequestHandler();
